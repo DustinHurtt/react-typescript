75c0b6ed5cfd7b1b38779e5d87cacef7a977223a
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-08-28 17:22:46 -0400

    added state, event, props, and classes examples

---
+++ src/props/Child.tsx
@@ -0,0 +1,20 @@
+interface ChildProps {
+    color: string;
+    onClick: () => void;
+    children: any
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+    return <div>
+        {color}
+        <button onClick={onClick}>Click me</button>
+        </div>
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick, children }) => {
+    return <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+    </div>
+}
+
